07a40260dd0dfcc8ceb5bb92d0b5f9de278461da
AugustZer0 <qweraz157890@gmail.com>
2025-09-10 10:57:49 +0900

    fix test

--- src/test/java/com/knusdp/SmartLedger/TestController.java
+++ src/test/java/com/knusdp/SmartLedger/TestController.java
@@ -3,14 +3,17 @@ package com.knusdp.SmartLedger;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TestController.class) // 컨트롤러만 테스트하도록 설정
+@AutoConfigureMockMvc(addFilters = false)
 class TestControllerTest {
 
     @Autowired // MockMvc 객체를 주입받아 사용
@@ -23,8 +26,9 @@ class TestControllerTest {
         String expectedMessage = "Hello World!";
 
         // when & then
-        mockMvc.perform(get("/hello")) // '/hello' 경로로 GET 요청을 보냄
-                .andExpect(status().isOk()) // HTTP 상태 코드가 200 (OK)인지 검증
-                .andExpect(content().string(expectedMessage)); // 응답 본문이 'Hello World!'인지 검증
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(expectedMessage));
+
     }
 }
