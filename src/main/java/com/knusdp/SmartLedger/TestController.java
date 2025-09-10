012324e6bb9c6b01edba459b8c3695808fe1268b
AugustZer0 <qweraz157890@gmail.com>
2025-09-10 11:10:33 +0900

    test

---
+++ src/main/java/com/knusdp/SmartLedger/TestController.java
@@ -0,0 +1,25 @@
+package com.knusdp.SmartLedger;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+
+@RequestMapping("/")
+
+class TestController {
+
+
+
+    @GetMapping("/hello")
+
+    public String helloWorld() {
+
+        return "Hello World!";
+
+    }
+
+}
