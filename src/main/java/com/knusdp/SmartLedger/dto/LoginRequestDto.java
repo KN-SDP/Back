682379e60f0a4d30be5ac84083bdb617fe2521a9
yoobin kim <yoobin0821@naver.com>
2025-09-15 18:52:08 +0900

    벡엔드

--- src/main/java/com/knusdp/SmartLedger/dto/LoginRequestDto.java
+++ src/main/java/com/knusdp/SmartLedger/dto/LoginRequestDto.java
@@ -8,9 +8,16 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class LoginRequestDto {
+    private Long userId;
     private String token;
     private String userEmail;
     private String userPassword;
+
+    public LoginRequestDto(Long userId, String email, String password, String token) {
+        this.token = token;
+        this.userId = userId;
+        this.userPassword = password;
+        this.userEmail = email;
+    }
 }
