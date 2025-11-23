1dea3e267550d51f8df317fbfe9160d07024b18e
yoobin kim <yoobin0821@naver.com>
2025-11-23 20:18:50 +0900

    fix/S3기능swagerUI설정

--- src/main/java/com/knusdp/SmartLedger/dto/GoalRequestSchema.java
+++ src/main/java/com/knusdp/SmartLedger/dto/GoalRequestSchema.java
@@ -1,11 +1,20 @@
 package com.knusdp.SmartLedger.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class GoalRequestSchema {
-    private CreateGoalRequestDto data;
+    @Schema(
+            description = "JSON 데이터 (CreateGoalRequestDto)",
+            type = "string",
+            example = "{\"title\":\"string\",\"targetAmount\":1,\"deadline\":\"2025-11-23\"}"
+    )
+    private String data; // ← 반드시 String이어야 함
+
+    @Schema(description = "업로드할 이미지 파일", type = "string", format = "binary")
     private MultipartFile image;
 }
+
 
