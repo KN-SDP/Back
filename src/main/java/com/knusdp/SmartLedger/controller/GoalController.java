3170ab57474368efae5908505ea4171a397d9bf5
yoobin kim <yoobin0821@naver.com>
2025-11-24 22:34:01 +0900

    fix/S3기능swagerUI설정

--- src/main/java/com/knusdp/SmartLedger/controller/GoalController.java
+++ src/main/java/com/knusdp/SmartLedger/controller/GoalController.java
@@ -14,7 +14,6 @@ import io.swagger.v3.oas.annotations.Operation;
 
 
 import io.swagger.v3.oas.annotations.Parameter;
-elop
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
