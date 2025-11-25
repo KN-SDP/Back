780cb3146f9bb1a9d3be6a7968663aa8dc57724f
yoobin02 <yoobin0821@naver.com>
2025-11-25 15:49:14 +0900

    Fix/s3#9 (#134)
    * fix/S3수정
    
    * fix/S3수정
    
    * fix/S3수정
    
    * fix/S3수정
    
    * fix/S3수정
    
    * fix/S3수정
    
    * fix/S3수정
    
    * fix/S3수정
    
    * fix/S3수정했음
    
    * fix/S3수정했음
    
    * fix/S3수정했음
    
    * fix/s3

--- src/main/java/com/knusdp/SmartLedger/config/OpenApiConfig.java
+++ src/main/java/com/knusdp/SmartLedger/config/OpenApiConfig.java
@@ -7,20 +7,9 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-
-import java.util.ArrayList;
 
 @Configuration
 public class OpenApiConfig {
-
-    public OpenApiConfig(MappingJackson2HttpMessageConverter converter) {
-        // application/octet-stream 미디어 타입 지원 추가
-        var supportedMediaTypes = new ArrayList<>(converter.getSupportedMediaTypes());
-        supportedMediaTypes.add(new MediaType("application", "octet-stream"));
-        converter.setSupportedMediaTypes(supportedMediaTypes);
-    }
 
     @Bean
     public OpenAPI openAPI() {
