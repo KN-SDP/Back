3297e12d141b503f91cb6cc3ec0af054b2c2ff90
AugustZer0 <qweraz157890@gmail.com>
2025-11-25 15:55:36 +0900

    Fix(Image Upload): 목표 가계부 이미지 업로드 관련
    # Conflicts:
    #	src/main/java/com/knusdp/SmartLedger/controller/GoalController.java
    #	src/main/java/com/knusdp/SmartLedger/dto/CreateGoalRequestDto.java

--- src/main/java/com/knusdp/SmartLedger/dto/CreateGoalRequestDto.java
+++ src/main/java/com/knusdp/SmartLedger/dto/CreateGoalRequestDto.java
@@ -1,15 +1,13 @@
 package com.knusdp.SmartLedger.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -17,24 +15,17 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class CreateGoalRequestDto {
 
-    @Schema(type = "string", example = "목표 제목")
-    @NotBlank
+    @NotBlank(message = "목표 제목은 필수입니다.")
     private String title;
 
-    @Schema(type = "number", example = "10000")
-    @NotNull
-    @DecimalMin("1")
+    @NotNull(message = "목표 금액은 필수입니다.")
+    @DecimalMin(value = "1", message = "목표 금액은 0보다 커야 합니다.")
     private BigDecimal targetAmount;
 
-    @Schema(type = "string", example = "2025-11-25")
-    @NotNull
-    @FutureOrPresent
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-
+    @NotNull(message = "마감일은 필수입니다.")
+    @FutureOrPresent(message = "마감일은 오늘 또는 미래여야 합니다.")
     private LocalDate deadline;
-
-    private MultipartFile image;
 }
-
